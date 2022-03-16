88a2e8254a9dc767a70fcc828b47f817d11ea836
Widyarso Joko Purnomo <widyjp10@gmail.com>
2022-03-17 02:23:43 +0700

    add bottom navigation bar

--- lib/features/home/view/home_page.dart
+++ lib/features/home/view/home_page.dart
@@ -3,8 +3,15 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:spooftify/main.dart';
 import 'package:spooftify/widgets/recent_history_card.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _bottomNavigationIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -88,9 +95,77 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ),
+              const SliverToBoxAdapter(
+                child: SizedBox(height: 16),
+              ),
+              SliverToBoxAdapter(
+                child: Text(
+                  'New Releases For You',
+                  style: GoogleFonts.gothicA1(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 150,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      final album = recentlyPlayed[index];
+
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                        child: SizedBox(
+                          width: 100,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                'assets/${album.cover}',
+                              ),
+                              Expanded(
+                                child: Text(
+                                  album.name,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    itemCount: recentlyPlayed.length,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _bottomNavigationIndex,
+        onTap: (index) {
+          setState(() {
+            _bottomNavigationIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_music),
+            label: 'Library',
+          ),
+        ],
       ),
     );
   }
